bc0c6716d7dd3483d359c17f87dd43e155bfb1a4
ayoakpata <akpata.ayobami@gmail.com>
2020-04-14 23:50:13 +0100

    minor fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoContainer from './components/TodoContainer';
 
-const element = <h1>Hello React</h1>;
-
 ReactDOM.render(<TodoContainer />, document.getElementById('root'));
