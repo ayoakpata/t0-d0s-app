726a3afb73e715497559cd138a9acdcb3854c155
ayoakpata <akpata.ayobami@gmail.com>
2020-04-21 23:46:48 +0100

    Updates

---
+++ precache-manifest.9910cfbc4eba888ca46810f716dbb2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff49d156ea4a35defd7776a13b0b5ff",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "09475593211a911b58dc",
+    "url": "/todos/static/css/main.6fb71b3c.chunk.css"
+  },
+  {
+    "revision": "6604dfa33fa994eed1b5",
+    "url": "/todos/static/js/2.a1207fb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos/static/js/2.a1207fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09475593211a911b58dc",
+    "url": "/todos/static/js/main.91d094b0.chunk.js"
+  },
+  {
+    "revision": "66d8fb93c3aa4d47c018",
+    "url": "/todos/static/js/runtime-main.54da4c53.js"
+  }
+]);
